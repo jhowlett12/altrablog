fd85094e2e3e86158c6d81c53a4acee04d211569
Jake Spurlock <jake_spurlock@wired.com>
2015-06-05 19:43:32 -0700

    Register the taxonomy.

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Add all of this code the functions.php
+ */
+
+
+/**
+ * Register the taxonomy of athletes.
+ * @return void
+ */
+function register_athlete_taxonomy() {
+
+	register_taxonomy( 'athlete', array( 'post' ), array(
+			'hierarchical'      => false,
+			'public'            => true,
+			'show_in_nav_menus' => true,
+			'show_ui'           => true,
+			'show_admin_column' => false,
+			'query_var'         => true,
+			'rewrite'           => true,
+			'capabilities'      => array(
+				'manage_terms'  => 'edit_posts',
+				'edit_terms'    => 'edit_posts',
+				'delete_terms'  => 'edit_posts',
+				'assign_terms'  => 'edit_posts'
+			),
+			'labels'            => array(
+				'name'                       => __( 'Athletes', 'altra' ),
+				'singular_name'              => _x( 'Athlete', 'taxonomy general name', 'altra' ),
+				'search_items'               => __( 'Search athletes', 'altra' ),
+				'popular_items'              => __( 'Popular athletes', 'altra' ),
+				'all_items'                  => __( 'All athletes', 'altra' ),
+				'parent_item'                => __( 'Parent athlete', 'altra' ),
+				'parent_item_colon'          => __( 'Parent athlete:', 'altra' ),
+				'edit_item'                  => __( 'Edit athlete', 'altra' ),
+				'update_item'                => __( 'Update athlete', 'altra' ),
+				'add_new_item'               => __( 'New athlete', 'altra' ),
+				'new_item_name'              => __( 'New athlete', 'altra' ),
+				'separate_items_with_commas' => __( 'Athletes separated by comma', 'altra' ),
+				'add_or_remove_items'        => __( 'Add or remove athletes', 'altra' ),
+				'choose_from_most_used'      => __( 'Choose from the most used athletes', 'altra' ),
+				'menu_name'                  => __( 'Athletes', 'altra' ),
+			),
+		)
+	);
+}
+
+// On init, register the taxonomy.
+add_action( 'init', 'kik_init' );
+
